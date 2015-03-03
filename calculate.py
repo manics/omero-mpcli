cf3fab226de1018fd542dbc3919c3192cf3cee49
Simon Li <spli@dundee.ac.uk>
2015-03-03 17:04:14 +0000

    Run omero import CLI in multiple-processes

--- calculate.py
+++ calculate.py
@@ -20,45 +20,33 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 """
-Client side calculation of features, stored locally.
-Uses multiprocessing to parallelise calculations on the local machine.
+Client side parallel OMERO commands.
 """
 
 import argparse
 import datetime
-import errno
 import getpass
 import logging
 import multiprocessing
-import numpy
-import os
 import pickle
 
 import omero
+import omero.cli
 import omero.gateway
-import portalocker
-
-import pychrm
-from pychrm.FeatureSet import Signatures
-from pychrm.PyImageMatrix import PyImageMatrix
 
 
-log = logging.getLogger('MultiCalc')
+log = logging.getLogger('mpomero')
 log.setLevel(logging.DEBUG)
 log.setLevel(logging.INFO)
 
 
-class CalculationException(Exception):
+class MpOmeroCliException(Exception):
 
     def __init__(self, msg):
-        super(CalculationException, self).__init__(msg)
+        super(MpOmeroCliException, self).__init__(msg)
 
 
-class FeatureFileAlreadyExists(CalculationException):
-    pass
-
-
-class Calculator(object):
+class MpOmeroCli(object):
 
     def __init__(self, host=None, port=None, user=None, password=None,
                  sessionid=None, groupid=-1, detach=True):
@@ -101,220 +89,20 @@ class Calculator(object):
             log.info('Closing session: %s', self.session)
         self.client.closeSession()
 
-    def imageGenerator(self, objs):
-        for o in objs:
-            log.info(o)
-            if isinstance(o, omero.gateway._ImageWrapper):
-                yield o
-            else:
-                children = o.listChildren()
-                for im in self.imageGenerator(children):
-                    yield im
 
-    def genObjects(self, typeids):
-        for (t, i) in typeids:
-            o = self.conn.getObject(t, i)
-            if not o:
-                raise CalculationException(
-                    'Unable to get object: %s %d' % (t, i))
-            yield o
-
-    def genComputationList(self, typeids):
-        """
-        typeids: A list of (TypeName, Id) pairs where TypeName is Image,
-        Dataset or Project
-        """
-        def planeParamsGen(im):
-            for c in xrange(im.getSizeC()):
-                for z in xrange(im.getSizeZ()):
-                    for t in xrange(im.getSizeT()):
-                        # yield (im.id, c, z, t, im.getSizeX(), im.getSizeY())
-                        yield (im.id, c, z, t)
-
-        objGen = self.genObjects(typeids)
-        for im in self.imageGenerator(objGen):
-            for params in planeParamsGen(im):
-                yield params
+def get_params_list(common, params, groupsize):
+    for i in xrange(0, len(params), groupsize):
+        yield list(common) + params[i:i + groupsize]
 
 
-def extractFeaturesPychrmSmall(conn, iid, c, z, t):
-    """
-    Calculate features for a single image plane. Note this takes in an
-    image id instead of an image object to support batch jobs where a list
-    of parameter sets can be provided.
-    """
-    im = conn.getObject('Image', iid)
-    if not im:
-        raise CalculationException('Image id not found: %d' % iid)
-
-    # Calculate features for a single plane (c/z/t)
-    pychrm_matrix = PyImageMatrix()
-    pychrm_matrix.allocate(im.getSizeX(), im.getSizeY())
-    numpy_matrix = pychrm_matrix.as_ndarray()
-
-    numpy_matrix[:] = im.getPrimaryPixels().getPlane(
-        theZ=z, theC=c, theT=t)
-    feature_plan = pychrm.StdFeatureComputationPlans.getFeatureSet()
-    options = ""  # Wnd-charm options
-    fts = Signatures.NewFromFeatureComputationPlan(
-        pychrm_matrix, feature_plan, options)
-
-    ft = {
-        'names': fts.names,
-        'values': fts.values,
-        'version': fts.version
-    }
-    return ft
+def invokecli(cmdline):
+    cli = omero.cli.CLI()
+    cli.loadplugins()
+    log.info('cmdline: %s', cmdline)
+    cli.invoke(cmdline)
 
 
-def meanIntensity(conn, iid, c, z, t):
-    """
-    Calculate features for a single image plane. Note this takes in an
-    image id instead of an image object to support batch jobs where a list
-    of parameter sets can be provided.
-    """
-    im = conn.getObject('Image', iid)
-    if not im:
-        raise CalculationException('Image id not found: %d' % iid)
-
-    # Calculate features for a single plane (c/z/t)
-    m = im.getPrimaryPixels().getPlane(theZ=z, theC=c, theT=t)
-    ft = {
-        'names': ['min', 'max', 'mean'],
-        'values': [m.min(), m.max(), m.mean()],
-        'version': '0'
-    }
-    return ft
-
-
-class FeatureFile(object):
-    """
-    A context manager to handle multiple concurrent feature calculations, and
-    saving features.
-
-    On entering the context manager an exclusive lockfile named after the image
-    parameters is created.
-    Features can be written to this file.
-    On exit the lockfile is renamed to the final output filename.
-    """
-
-    def __init__(self, iid, c, z, t):
-        self.dir = 'SmallFeatureSet'
-        self.dir = os.path.join(self.dir, 'image%08d' % iid)
-        self.filename = 'image%08d-c%04d-z%04d-t%04d' % (iid, c, z, t)
-        self.npy = os.path.join(self.dir, self.filename + '.npy')
-        self.saved = False
-
-        try:
-            os.makedirs(self.dir)
-        except OSError as e:
-            if e.errno != errno.EEXIST or not os.path.isdir(self.dir):
-                raise
-
-    def __enter__(self):
-        """
-        Succeeds if an exclusive lock was obtained and the final output file
-        does not exist or is empty, otherwise raises an exception.
-        """
-        lock = os.path.join(self.dir, self.filename + '.tmp')
-        log.debug('Locking: %s', lock)
-        self.fh = open(lock, 'w+')
-        try:
-            portalocker.lock(self.fh, portalocker.LOCK_EX)
-            # Check whether the final output file has already been created
-            try:
-                log.debug('Checking: %s', self.npy)
-                # Using with open seems to trigger an ipython bug
-                # with open(self.npy, 'r') as f:
-                f = open(self.npy, 'r')
-                try:
-                    f.seek(0, 2)
-                    if f.tell() > 0:
-                        raise FeatureFileAlreadyExists(
-                            'Feature file already exists: %s (%d B)' % (
-                                self.npy, f.tell()))
-                finally:
-                    f.close()
-            except IOError:
-                # Assume this is file not found
-                pass
-            except:
-                os.unlink(self.fh.name)
-                raise
-        except:
-            self.fh.close()
-            raise
-        return self
-
-    def save(self, a):
-        log.debug('Saving: %s', self.fh.name)
-        numpy.save(self.fh, a)
-        self.saved = True
-
-    def __exit__(self, type, value, traceback):
-        if self.saved:
-            log.debug('Renaming: %s->%s', self.fh.name, self.npy)
-            os.rename(self.fh.name, self.npy)
-            log.info('Saved: %s', self.npy)
-        else:
-            log.debug('Empty, deleting: %s', self.fh.name)
-            os.unlink(self.fh.name)
-        self.fh.close()
-
-
-class FeatureFileZ(FeatureFile):
-    """
-    FeatureFile that saves to numpy archives instead of single arrays
-    """
-    def __init__(self, iid, c, z, t):
-        super(FeatureFileZ, self).__init__(iid, c, z, t)
-        self.npy = os.path.join(self.dir, self.filename + '.npz')
-
-    def save(self, **kwargs):
-        log.debug('Saving: %s', self.fh.name)
-        numpy.savez(self.fh, **kwargs)
-        self.saved = True
-
-
-# calculate = meanIntensity
-calculate = extractFeaturesPychrmSmall
-"""
-calculate should be a function that calculates features given a single image
-plane.
-Note this takes in an image id instead of an image object to support batch
-jobs where a list of parameter sets can be provided.
-
-Function of the form r = func(conn, iid, c, z, t)
-  conn: BlitzGateway object
-  iid: Image ID
-  c, z, t: Index of the C/Z/T plance
-  r: A dict with fields names ([string]), values ([double]), and
-    version (string)
-"""
-
-
-def run1(params):
-    log.info('params: %s', params)
-    try:
-        ftparams = params.pop('ftparams')
-        # with FeatureFile(*ftparams) as ff:
-        with FeatureFileZ(*ftparams) as ff:
-            with Calculator(**params) as c:
-                log.info('Calculating features')
-                feats = calculate(c.conn, *ftparams)
-                log.debug(feats)
-                # ff.save(feats['values'])
-                ff.save(**feats)
-                return 'Completed: %s' % str(ftparams)
-    except FeatureFileAlreadyExists as e:
-        log.error(e)
-        return 'Already exists: %s' % e
-    except Exception as e:
-        log.error(e, exc_info=True)
-        return 'Failed: %s' % e
-
-
-def main(args):
+def main(args, common, params):
     logging.basicConfig(format='%(asctime)s %(levelname)-5.5s %(message)s')
 
     log.info(args)
@@ -322,50 +110,35 @@ def main(args):
     out = 'out-%s.pkl' % (datetime.datetime.strftime(
         datetime.datetime.now(), '%Y%m%d-%H%M%S'))
 
-    # items = [('Dataset', 1802), ('Project', 1014)]
-
-    with Calculator(args.server, args.port, args.user, password=args.password,
+    with MpOmeroCli(args.server, args.port, args.user, password=args.password,
                     detach=False) as c:
-        # Remember complist is a generator
-        complist = c.genComputationList(args.ordered_arguments)
+        sessionid = c.client.getSessionId()
+        cmd = common
+        if args.login:
+            cmd + ['-s', c.client.getProperty('omero.host'), '-p',
+                   c.client.getProperty('omero.port'), '-k', sessionid]
+        print cmd, params, args.groupsize
+        paramslist = get_params_list(cmd, params, args.groupsize)
 
         if args.dry_run:
-            log.info('Computation list:')
-            for p in complist:
+            log.info('Command list:')
+            for p in paramslist:
                 log.info('  %s', p)
             return
 
-        sessionid = c.client.getSessionId()
-
-        paramsets = [{'host': args.server, 'port': args.port,
-                      'sessionid': sessionid, 'ftparams': p} for p in complist]
-        log.debug('paramsets: %s', [p['ftparams'] for p in paramsets])
-
         log.info('Creating pool of %d threads', args.threads)
         pool = multiprocessing.Pool(args.threads)
-        results = pool.map(run1, paramsets)
+        results = pool.map(invokecli, paramslist)
 
-        log.info('Saving statuses to: %s', out)
+        log.info('Saving results to: %s', out)
         with open(out, 'wb') as f:
             pickle.dump(results, f)
-        # log.info('pool.imap results: %s', results)
         log.info('pool.map results length: [%d]', len(results))
 
     log.info('Main thread exiting')
 
 
 def parse_args(args=None):
-    class StoreOrdered(argparse.Action):
-        """
-        Based on http://stackoverflow.com/a/9028031
-        """
-        ORDERED_DEST = 'ordered_arguments'
-
-        def __call__(self, parser, namespace, values, option_string=None):
-            v = getattr(namespace, self.ORDERED_DEST, [])
-            v.append((self.dest, values))
-            setattr(namespace, self.ORDERED_DEST, v)
-
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--server', default=None)
@@ -377,11 +150,27 @@ def parse_args(args=None):
         '--threads', default=multiprocessing.cpu_count(), type=int)
 
     parser.add_argument('-n', '--dry-run', action='store_true')
+    parser.add_argument('--groupsize', default=1, type=int,
+                        help='Pass args in groups of this size')
+    parser.add_argument('--login', action='store_true',
+                        help='Include session login arguments in command')
 
-    parser.add_argument('-p', action=StoreOrdered, dest='Project', type=int)
-    parser.add_argument('-d', action=StoreOrdered, dest='Dataset', type=int)
-    parser.add_argument('-i', action=StoreOrdered, dest='Image', type=int)
-    return parser.parse_args(args)
+    parser.add_argument('other', nargs=argparse.REMAINDER)
+
+    args = parser.parse_args(args)
+    common = []
+    params = []
+
+    gotsep = False
+    for arg in args.other:
+        if gotsep:
+            params.append(arg)
+        elif arg == '--':
+            gotsep = True
+        else:
+            common.append(arg)
+
+    return args, common, params
 
 
 if __name__ == '__main__':
@@ -389,5 +178,9 @@ if __name__ == '__main__':
     try:
         __IPYTHON__
     except NameError:
-        args = parse_args()
-        main(args)
+        allargs = parse_args()
+        main(*allargs)
+
+# Example:
+# calculate.py --server server --user user --password password --groupsize 3 \
+#     import -d 1 -- a.img b.img c.img ...
